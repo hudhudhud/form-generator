01507d7b77ce4dd9a209ada86e82dc12092a589c
hd <996810472@qq.com>
2021-02-26 16:31:24 +0800

    调整 loadBeautifier

--- src/utils/loadBeautifier.js
+++ src/utils/loadBeautifier.js
@@ -9,22 +9,22 @@ export default function loadBeautifier(cb) {
     return
   }
 
-  const loading = ELEMENT.Loading.service({
-    fullscreen: true,
-    lock: true,
-    text: '格式化资源加载中...',
-    spinner: 'el-icon-loading',
-    background: 'rgba(255, 255, 255, 0.5)'
-  })
+  // const loading = ELEMENT.Loading.service({
+  //   fullscreen: true,
+  //   lock: true,
+  //   text: '格式化资源加载中...',
+  //   spinner: 'el-icon-loading',
+  //   background: 'rgba(255, 255, 255, 0.5)'
+  // })
 
   // loadScript('https://lib.baomitu.com/js-beautify/1.10.2/beautifier.min.js', () => {
-  loadScript('http://mobileproxy.h3c.com:8000/dev/form-generator/js-beautiful/beautifier.min.js', () => {
-    loading.close()
-    // eslint-disable-next-line no-undef
-    beautifierObj = beautifier
-    cb(beautifierObj)
-  })
+  // loadScript('http://mobileproxy.h3c.com:8000/dev/form-generator/js-beautiful/beautifier.min.js', () => {
+  //   loading.close()
+  //   // eslint-disable-next-line no-undef
+  //   beautifierObj = beautifier
+  //   cb(beautifierObj)
+  // })
 
-  // beautifierObj = beautifier
-  // cb(beautifierObj)
+  beautifierObj = beautifier
+  cb(beautifierObj)
 }
